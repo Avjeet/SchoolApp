16ce0302d747f46ebd0ba806fb1365cb20fb9038
avjeetdaphnislabs <singh.avjeet02@gmail.com>
2019-01-22 19:18:38 +0530

    Added payment complete activity

---
+++ app/src/main/java/com/labs/daphnis/schoolapp/SchoolFees/PaymentCompleteActivity.kt
@@ -0,0 +1,13 @@
+package com.labs.daphnis.schoolapp.SchoolFees
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.labs.daphnis.schoolapp.R
+
+class PaymentCompleteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_complete)
+    }
+}
--- app/src/main/java/com/labs/daphnis/schoolapp/SchoolFees/PaymentModeActivity.kt
+++ app/src/main/java/com/labs/daphnis/schoolapp/SchoolFees/PaymentModeActivity.kt
@@ -1,9 +1,11 @@
 package com.labs.daphnis.schoolapp.SchoolFees
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import com.labs.daphnis.schoolapp.R
+import kotlinx.android.synthetic.main.activity_payment_mode.*
 
 class PaymentModeActivity : AppCompatActivity() {
 
@@ -13,6 +15,10 @@ class PaymentModeActivity : AppCompatActivity() {
 
         supportActionBar?.title= getString(R.string.school_fees)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        image_pay.setOnClickListener{
+            startActivity(Intent(this@PaymentModeActivity,PaymentCompleteActivity::class.java))
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
